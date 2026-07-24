b4b1d0ea4f3d93f1ee2738f4f077baa2cc789223
ahmedibm9-cyber <ahmedibm9-cyber@users.noreply.github.com>
2026-07-24 07:25:54 +0300

    fix: add unpkg.com to CSP script-src for Leaflet CDN

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -38,7 +38,7 @@ class SecurityHeaders
         // TODO: migrate to nonce-based CSP when Livewire supports it
         $csp = implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net",
             "img-src 'self' data: blob: https://*.tile.openstreetmap.org",
             "font-src 'self' data: https://fonts.googleapis.com https://fonts.gstatic.com https://fonts.bunny.net",
