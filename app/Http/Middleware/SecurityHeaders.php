fd959fb538b498eaab20189d36835a334818f589
ahmedibm9-cyber <ahmedibm9-cyber@users.noreply.github.com>
2026-07-24 07:45:51 +0300

    fix: add unpkg.com to CSP img-src for Leaflet marker icons

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -40,7 +40,7 @@ class SecurityHeaders
             "default-src 'self'",
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net",
-            "img-src 'self' data: blob: https://*.tile.openstreetmap.org",
+            "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://unpkg.com",
             "font-src 'self' data: https://fonts.googleapis.com https://fonts.gstatic.com https://fonts.bunny.net",
             "connect-src 'self' wss: ws:",
             "frame-ancestors 'none'",
